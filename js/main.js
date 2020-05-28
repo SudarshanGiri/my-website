ba6a7daf1429d1bc2337877af8efc4bf2f521ec9
Sudarshan Giri <41857483+SudarshanGiri@users.noreply.github.com>
2020-05-28 14:10:36 +0545

    final commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).on("click", "ul li", function () {
+  $(this).addClass("active").siblings().removeClass("active");
+});
+
+$(window).scroll(function () {
+  $("nav").toggleClass("scrolled", $(this).scrollTop() > 900);
+});
+$("#menu").onePageNav({
+  currentClass: "active",
+  changeHash: false,
+  scrollSpeed: 500,
+  scrollThreshold: 0.5,
+  filter: "",
+  easing: "swing",
+});
+//hide toggle menu on click :
+$(".navbar-nav>li>a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
+//carousel transition
+$(document).ready(function () {
+  jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 3000; // 3 seconds
+});
+
+//preloader
+$(document).ready(function ($) {
+  var Body = $("body");
+  Body.addClass("preloader-site");
+});
+$(window).on("load", function () {
+  $(".preloader-wrapper").fadeOut(2000);
+  $("body").removeClass("preloader-site");
+});
